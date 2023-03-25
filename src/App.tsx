132974f04e9d0cc46911a578ab68c081a689503d
Jithin K George <jithin.george@qburst.com>
2023-03-25 12:33:18 +0530

    demo for experience page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Experience from "./Components/Experience/Experience";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="bg-white">
       <p className="text-6xl w-auto h-auto">Let's start</p>
+      <Experience/>
     </div>
   );
 }
