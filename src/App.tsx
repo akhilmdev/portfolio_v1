7deef9e03a0d71bffa33ab2679e2d8c3f4f4fceb
Jithin K George <jithin.george@qburst.com>
2023-03-25 16:08:48 +0530

    add basic text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Experience from "./Components/Experience/Experience";
+import Experience from "./components/Experience/Experience";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +8,7 @@ function App() {
   return (
     <div className="bg-white">
       <p className="text-6xl w-auto h-auto">Let's start</p>
-      <Experience/>
+      <Experience />
     </div>
   );
 }
