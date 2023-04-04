0f7f37e76afd9c01d868c845f4bf41359c0772a1
mad-akhil <akhilm@yeswearemad.com>
2023-04-04 15:38:14 +0530

    scroll to top added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import ScrollTop from "./components/common/scrollTop";
 import Experience from "./components/Experience/Experience";
 import Home from "./components/Home/Home";
 
 function App() {
+  const [height, setHeight] = useState(window.scrollY);
+
+  window.onscroll = function () {
+    setHeight(window.scrollY);
+  };
+
   return (
     <div className="h-screen w-full bg-white relative">
       <Home />
       <Experience cols="3" />
+      <ScrollTop height={height} />
     </div>
   );
 }
---
+++ src/components/common/scrollTop.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  height: number;
+}
+const ScrollTop: React.FC<Props> = ({ height }) => {
+  console.log(height, "======");
+  return (
+    <div
+      className={"fixed bottom-4 right-4 cursor-pointer hidden md:inline"}
+      style={height > 800 ? { display: "block" } : { display: "none" }}
+      onClick={() =>
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        })
+      }
+    >
+      <img src="../assets/scroll-up.png" alt="scroll to top" />
+    </div>
+  );
+};
+
+export default ScrollTop;
